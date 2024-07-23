084af19584cbc366c1321a6b7a097743c0fc3cde
Tejas5k <167913819+Tejas5k@users.noreply.github.com>
2024-07-24 01:08:09 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,12 @@
+class add
+{
+    public static void main(String a[])
+    {
+        int num1 = 12;
+        int num2 = 13;
+        int result = num1 + num2;
+        System.out.println("Addition is :"+ result);
+        int res = num1-num2;
+        System.out.println("sub is :"+ res);
+    }
+}
---
+++ booleanex.java
@@ -0,0 +1,10 @@
+class booleanex
+{
+    public static void main(String a[])
+    {
+       int x=21;
+       int y=20;
+       boolean result = x>y;
+       System.out.print(result);
+    }
+}
